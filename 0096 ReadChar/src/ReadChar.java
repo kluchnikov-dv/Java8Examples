21d9cbb515ea69d42f317218e44b3b2272e8c7dc
Kluchnikov.DV <Kluchnikov.DV@gmail.com>
2017-04-11 15:35:03 +0300

    Глава 13 Чтение символов
    стр. 360

--- 0096 ReadChar/src/ReadChar.java
+++ 0096 ReadChar/src/ReadChar.java
@@ -12,12 +12,13 @@ public class ReadChar {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		char ch;
-		System.out.println("Введите символы, q для выхода");
+		System.out.println("Введите символы, q для выхода: ");
 		//Читаем и выводим символы пока пользователь не ввел q
 		do {
 			ch = (char)br.read();
 			System.out.println(ch);			
 		} while ( ch != 'q');
+		System.out.println("Выход.");
 		
 	}
 
