f7e86b16c431ca6731f935cad51de4e5acac4cad
Kluchnikov.DV <Kluchnikov.DV@gmail.com>
2017-04-13 10:54:16 +0300

    Глава 13 Запись данных, вывод на консоль
    стр. 363

---
+++ 0099 WriteDemo/src/WriteDemo.java
@@ -0,0 +1,17 @@
+//Глава 13
+//Запись данных, вывод на консоль
+//стр. 363
+
+
+public class WriteDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int b;
+		b = 'A';
+		System.out.write(b);
+		System.out.write('\n');
+
+	}
+
+}
